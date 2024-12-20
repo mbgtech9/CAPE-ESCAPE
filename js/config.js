823b6c51131dc00155a809c1ed0178c82466760e
mbgtech9 <114033810+mbgtech9@users.noreply.github.com>
2024-12-20 00:31:08 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-west-2_AtFkzn0MF', // e.g. us-west-2_AtFkzn0MF
-        userPoolClientId: '7aisnk3shse8918f2ulpteapeh', // 7aisnk3shse8918f2ulpteapeh
-        region: 'us-west-2' //  e.g. us-west-2
+        userPoolId: 'us-west-2_AtFkzn0MF', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7aisnk3shse8918f2ulpteapeh', // 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // Replace with your actual API Gateway invoke URL
