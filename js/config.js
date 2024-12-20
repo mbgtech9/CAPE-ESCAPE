4015f7de25ae9578515dd25bdb026af581372b4e
mbgtech9 <114033810+mbgtech9@users.noreply.github.com>
2024-12-19 23:15:32 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,23 @@
-window._config = {
-    cognito: {
-        userPoolId: 'us-west-2_AtFkzn0MF', // e.g. us-west-2_AtFkzn0MF 
-        userPoolClientId: '7aisnk3shse8918f2ulpteapeh', // e.g. 7aisnk3shse8918f2ulpteapeh
-        region: 'us-west-2' // e.g. us-west-2
-    },
-    api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-    }
-};
+(function () {
+    const getConfig = () => {
+        const config = {
+            cognito: {
+                userPoolId: process.env.COGNITO_USER_POOL_ID || 'us-west-2_AtFkzn0MF',
+                userPoolClientId: process.env.COGNITO_USER_POOL_CLIENT_ID || '7aisnk3shse8918f2ulpteapeh',
+                region: process.env.COGNITO_REGION || 'us-west-2',
+            },
+            api: {
+                invokeUrl: process.env.API_INVOKE_URL || '', // Add your default or fallback URL if necessary
+            },
+        };
+
+        // Validate configuration values
+        if (!config.cognito.userPoolId || !config.cognito.userPoolClientId || !config.cognito.region) {
+            throw new Error('Cognito configuration is incomplete.');
+        }
+
+        return config;
+    };
+
+    window._config = getConfig();
+})();
