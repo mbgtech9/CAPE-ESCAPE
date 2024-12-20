5526c844e273594bfb682cee0b6ce519a449355c
mbgtech9 <114033810+mbgtech9@users.noreply.github.com>
2024-12-20 00:06:21 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-west-2_AtFkzn0MF', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7aisnk3shse8918f2ulpteapeh', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-west-2' // e.g. us-east-2
+        userPoolId: 'us-west-2_AtFkzn0MF', // us-west-2_AtFkzn0MF
+        userPoolClientId: '7aisnk3shse8918f2ulpteapeh', // 7aisnk3shse8918f2ulpteapeh
+        region: 'us-west-2' // Replace with your actual region
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod
+        invokeUrl: '' // Replace with your actual API Gateway invoke URL
     }
 };
+
