a9806bcce3211454d94525928362ffcb61353b3d
mbgtech9 <114033810+mbgtech9@users.noreply.github.com>
2024-12-20 21:47:21 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -9,6 +9,13 @@ window._config = {
     }
 };
 
-window._config = { cognito: { userPoolId: 'us-west-2_AtFkzn0MF', // e.g. us-east-2_uXboG5pAb userPoolClientId: '7aisnk3shse8918f2ulpteapeh', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv region: 'us-west-2' // e.g. us-east-2 }, api: { invokeUrl: 'https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod } };
-
-                             window._config = { cognito: { userPoolId: 'us-west-2_AtFkzn0MF', // e.g. us-east-2_uXboG5pAb userPoolClientId: '7aisnk3shse8918f2ulpteapeh', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv region: 'us-west-2' // e.g. us-east-2 }, api: { invokeUrl: 'https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod } };
+window._config = {
+    cognito: {
+        userPoolId: 'us-west-2_AtFkzn0MF', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7aisnk3shse8918f2ulpteapeh', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod
+    }
+};
